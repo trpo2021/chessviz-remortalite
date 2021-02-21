73f68f083d720d4b539d54e0705d5cf1fbd1de9d
Ignat Mora <morrig407@gmail.com>
2021-02-21 19:00:34 +0700

    Add print of spaces if cell is empty

--- main.c
+++ main.c
@@ -13,7 +13,8 @@ void printBoard()
 {
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
-            printf("%c ", board[i][j]);
+	    if (board[i][j]) printf("%c ", board[i][j]);
+	    else printf("%c ", ' ');
         }
         putchar('\n');
     }
