d45afce9671f5e2d2b26a52da4983d1188139698
Ignat Mora <morrig407@gmail.com>
2021-03-02 13:08:03 +0700

    Rename functions, add macro BOARD_SIZE

--- main.c
+++ main.c
@@ -7,10 +7,12 @@
 #define QUEEN 113
 #define KING 107
 
-void printBoard(int board[8][8])
+#define BOARD_SIZE 8
+
+void print_board(int board[BOARD_SIZE][BOARD_SIZE])
 {
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        for (int j = 0; j < BOARD_SIZE; j++) {
             if (board[i][j])
                 printf("%c ", board[i][j]);
             else
@@ -20,53 +22,53 @@ void printBoard(int board[8][8])
     }
 }
 
-int makeFigure(int figure, int isWhite)
+int make_figure(int figure, int isWhite)
 {
     return figure - isWhite * 32;
 }
 
-void makeBoard(int board[8][8])
+void make_board(int board[BOARD_SIZE][BOARD_SIZE])
 {
-    board[0][0] = makeFigure(ROOK, 0);
-    board[0][1] = makeFigure(KNIGHT, 0);
-    board[0][2] = makeFigure(BISHOP, 0);
-    board[0][3] = makeFigure(QUEEN, 0);
-    board[0][4] = makeFigure(KING, 0);
-    board[0][5] = makeFigure(BISHOP, 0);
-    board[0][6] = makeFigure(KNIGHT, 0);
-    board[0][7] = makeFigure(ROOK, 0);
+    board[0][0] = make_figure(ROOK, 0);
+    board[0][1] = make_figure(KNIGHT, 0);
+    board[0][2] = make_figure(BISHOP, 0);
+    board[0][3] = make_figure(QUEEN, 0);
+    board[0][4] = make_figure(KING, 0);
+    board[0][5] = make_figure(BISHOP, 0);
+    board[0][6] = make_figure(KNIGHT, 0);
+    board[0][7] = make_figure(ROOK, 0);
 
-    board[1][0] = makeFigure(PAWN, 0);
-    board[1][1] = makeFigure(PAWN, 0);
-    board[1][2] = makeFigure(PAWN, 0);
-    board[1][3] = makeFigure(PAWN, 0);
-    board[1][4] = makeFigure(PAWN, 0);
-    board[1][5] = makeFigure(PAWN, 0);
-    board[1][6] = makeFigure(PAWN, 0);
-    board[1][7] = makeFigure(PAWN, 0);
+    board[1][0] = make_figure(PAWN, 0);
+    board[1][1] = make_figure(PAWN, 0);
+    board[1][2] = make_figure(PAWN, 0);
+    board[1][3] = make_figure(PAWN, 0);
+    board[1][4] = make_figure(PAWN, 0);
+    board[1][5] = make_figure(PAWN, 0);
+    board[1][6] = make_figure(PAWN, 0);
+    board[1][7] = make_figure(PAWN, 0);
 
-    board[7][0] = makeFigure(ROOK, 1);
-    board[7][1] = makeFigure(KNIGHT, 1);
-    board[7][2] = makeFigure(BISHOP, 1);
-    board[7][3] = makeFigure(QUEEN, 1);
-    board[7][4] = makeFigure(KING, 1);
-    board[7][5] = makeFigure(BISHOP, 1);
-    board[7][6] = makeFigure(KNIGHT, 1);
-    board[7][7] = makeFigure(ROOK, 1);
+    board[7][0] = make_figure(ROOK, 1);
+    board[7][1] = make_figure(KNIGHT, 1);
+    board[7][2] = make_figure(BISHOP, 1);
+    board[7][3] = make_figure(QUEEN, 1);
+    board[7][4] = make_figure(KING, 1);
+    board[7][5] = make_figure(BISHOP, 1);
+    board[7][6] = make_figure(KNIGHT, 1);
+    board[7][7] = make_figure(ROOK, 1);
 
-    board[6][0] = makeFigure(PAWN, 1);
-    board[6][1] = makeFigure(PAWN, 1);
-    board[6][2] = makeFigure(PAWN, 1);
-    board[6][3] = makeFigure(PAWN, 1);
-    board[6][4] = makeFigure(PAWN, 1);
-    board[6][5] = makeFigure(PAWN, 1);
-    board[6][6] = makeFigure(PAWN, 1);
-    board[6][7] = makeFigure(PAWN, 1);
+    board[6][0] = make_figure(PAWN, 1);
+    board[6][1] = make_figure(PAWN, 1);
+    board[6][2] = make_figure(PAWN, 1);
+    board[6][3] = make_figure(PAWN, 1);
+    board[6][4] = make_figure(PAWN, 1);
+    board[6][5] = make_figure(PAWN, 1);
+    board[6][6] = make_figure(PAWN, 1);
+    board[6][7] = make_figure(PAWN, 1);
 }
 
 int main()
 {
-    int board[8][8] = {};
-    makeBoard(board);
-    printBoard(board);
+    int board[BOARD_SIZE][BOARD_SIZE] = {};
+    make_board(board);
+    print_board(board);
 }
